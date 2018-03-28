572855c1807b9d903124434f85d4aad2e223cd8d
逝者如斯 <413050636@qq.com>
2018-03-28 08:56:37 +0800

    阿里三方 2018年3月28日 08:56:31

---
+++ src/main/java/com/youge/yogee/interfaces/util/BankUtil.java
@@ -0,0 +1,67 @@
+package com.youge.yogee.interfaces.util;
+
+
+import com.youge.yogee.common.mapper.JsonMapper;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by liyuan on 2018/3/27.
+ */
+public class BankUtil {
+
+    public static void main(String[] args) {
+    }
+
+    private static final String appcode = "56366a54460b491aba7cba9cc5696f6e";
+
+    public static Boolean bankVerify(String bankcard,String idcard,String mobile,String name){
+        String host = "https://bankcard4.shumaidata.com";
+        String path = "/bank_card4/verify";
+        String method = "POST";
+        Map<String, String> headers = new HashMap<>();
+        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+        headers.put("Authorization", "APPCODE " + appcode);
+        //根据API的要求，定义相对应的Content-Type
+        headers.put("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        Map<String, String> querys = new HashMap<>();
+        Map<String, String> bodys = new HashMap<>();
+        bodys.put("bankcard", bankcard);
+        bodys.put("idcard", idcard);
+        bodys.put("mobile", mobile);
+        bodys.put("name", name);
+
+
+
+        try {
+            /**
+             * 重要提示如下:
+             * HttpUtils请从
+             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/src/main/java/com/aliyun/api/gateway/demo/util/HttpUtils.java
+             * 下载
+             *
+             * 相应的依赖请参照
+             * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
+             */
+            HttpResponse response = HttpUtils.doPost(host, path, method, headers, querys, bodys);
+            System.out.println(response.toString());
+            //获取response的body
+            String s = EntityUtils.toString(response.getEntity());
+            Map<String ,Object> access_token = JsonMapper.getInstance().fromJson(s, Map.class );
+            Map<String ,Object> b = (Map<String ,Object>)(access_token.get("result"));
+            if(b.get("res").toString().equals("1")){
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return false;
+    }
+
+
+
+}
--- src/main/java/com/youge/yogee/interfaces/util/SMSUtil.java
+++ src/main/java/com/youge/yogee/interfaces/util/SMSUtil.java
@@ -1,5 +1,11 @@
 package com.youge.yogee.interfaces.util;
 
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
 import com.youge.yogee.common.cloopen.sdk.CCPRestSDK;
 import com.youge.yogee.common.config.Global;
 import org.springframework.stereotype.Service;
@@ -16,12 +22,13 @@ public class SMSUtil {
 
     /**
      * 发送短信
+     *
      * @param telephone 手机号
-     * @param template 短信模板
-     * @param code 验证码
-     * @param minute 有效时间
+     * @param template  短信模板
+     * @param code      验证码
+     * @param minute    有效时间
      */
-    public static boolean sendSMS(String telephone, String template, String code, String minute){
+    public static boolean sendSMS(String telephone, String template, String code, String minute) {
 
         HashMap<String, Object> result = null;
         boolean flag = false;
@@ -32,26 +39,50 @@ public class SMSUtil {
         restAPI.init("app.cloopen.com", "8883");// 初始化服务器地址和端口，格式如下，服务器地址不需要写https://
         restAPI.setAccount(Global.getConfig("cloopen.account"), Global.getConfig("cloopen.token"));// 初始化主帐号和主帐号TOKEN
         restAPI.setAppId(Global.getConfig("cloopen.apply.id"));// 应用ID
-        result = restAPI.sendTemplateSMS(telephone, "161317", new String[]{code, minute+"分钟"});
+        result = restAPI.sendTemplateSMS(telephone, "161317", new String[]{code, minute + "分钟"});
 
 //        System.out.println("SDKTestSendTemplateSMS result=" + result);
 
-        if("000000".equals(result.get("statusCode"))){
+        if ("000000".equals(result.get("statusCode"))) {
             //正常返回输出data包体信息（map）
-            HashMap<String,Object> data = (HashMap<String, Object>) result.get("data");
+            HashMap<String, Object> data = (HashMap<String, Object>) result.get("data");
             Set<String> keySet = data.keySet();
-            for(String key:keySet){
+            for (String key : keySet) {
                 Object object = data.get(key);
 //                System.out.println(key +" = "+object);
             }
             flag = true;
-        }else{
+        } else {
             //异常返回输出错误码和错误信息
-            System.out.println("错误码=" + result.get("statusCode") +" 错误信息= "+result.get("statusMsg"));
+            System.out.println("错误码=" + result.get("statusCode") + " 错误信息= " + result.get("statusMsg"));
             flag = false;
         }
 
         return flag;
+    }
+
+    /***
+     *
+     * @param phoneNumbers 必填:待发送手机号
+     * @param templateCode 必填:短信模板-可在短信控制台中找到
+     *                                                  登录确认验证码-SMS_129595016
+     *                                                  修改密码验证码-SMS_129595013
+     *                                                  信息变更验证码-SMS_129595012
+     * @param templateParam 可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"
+     * @return 发送成功返回 true
+     */
+    public static boolean sendAliSMS(String phoneNumbers, String templateCode, String templateParam) {
+
+        try {
+            SendSmsResponse response = AliSMSUtil.sendSms(phoneNumbers,  templateCode, templateParam);
+            if(response.getCode() != null && response.getCode().equals("OK")) {
+                return true;
+            }
+        } catch (ClientException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
