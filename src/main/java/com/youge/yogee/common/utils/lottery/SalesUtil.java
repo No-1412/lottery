71659974f8b2a2cff71963af0f202a67e05cdadb
sxc <434453789@qq.com>
2018-08-31 17:23:56 +0800

    修改

---
+++ src/main/java/com/youge/yogee/common/utils/lottery/SalesUtil.java
@@ -0,0 +1,15 @@
+package com.youge.yogee.common.utils.lottery;
+
+import com.youge.yogee.common.utils.DateUtils;
+
+import java.util.Date;
+
+public class SalesUtil {
+    public static boolean isInDate() {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isInDate());
+    }
+}
